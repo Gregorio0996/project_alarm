d466b7599cfec513606586a72818987b3348236d
Gregorio1809 <gregorioimanuel@gmail.com>
2018-08-09 09:56:16 +0700

    fixing postpone code

--- project_alarm/lcd_clock/lcd_clock.ino
+++ project_alarm/lcd_clock/lcd_clock.ino
@@ -13,13 +13,13 @@
 #define WEEKDAY 3
 
 //weekday state:
-#define SUN 0
-#define MON 1
-#define TUE 2
-#define WED 3
-#define THU 4
-#define FRI 5
-#define SAT 6
+#define MIN 0
+#define SEN 1
+#define SEL 2
+#define RAB 3
+#define KAM 4
+#define JUM 5
+#define SAB 6
 
 //Time:
 #define HOURS 0
@@ -51,6 +51,8 @@ byte menustate = 0;
 byte al_hour = 0;
 byte al_min = 0;
 byte al_sec = 0;
+byte pp = 3;
+byte postmin = 0;
 String stat_alarm = "OFF";
 bool act_alarm = false;
 
@@ -65,7 +67,6 @@ void setup() {
   //initialize I2C and lcd:
   lcd.begin(16, 2);
   Wire.begin();
-  // Set the cursor at the begining of the first row
   /*lcd.setCursor(0, 0);
 
     // Print a text in the first row
@@ -85,9 +86,6 @@ void loop() {
   printTime();
   // Listen for buttons for 1 second
   buttonListen();
-
-
-
 
 }
 
@@ -236,8 +234,6 @@ void buttonListen() {
               case MINUTES:
                 al_min--;
                 if (al_min == -1) al_min = 59;
-
-
             }
         }
         break;
@@ -273,7 +269,7 @@ void buttonListen() {
   }
 }
 
-// Print the current setting
+
 void printSetting() {
   int button = lcd.button();
   int tempC = DS3231_get_treg();
@@ -326,40 +322,52 @@ void printSetting() {
       break;
 
     case IDLEMENU:
+      postmin = minutes + 1;
       if (hours == al_hour && minutes == al_min && act_alarm == true) {
         lcd.clear();
         lcd.setCursor(1, 0);
-        lcd.print("Wake up!!!");
-        lcd.setCursor(1, 1);
-        lcd.print("Wake up!!!");
-        digitalWrite (buzzerPin, HIGH);
-        delay (100);
-        digitalWrite (buzzerPin, LOW);
-        delay (1000);
-        if (button == KEYPAD_DOWN) {
-          stat_alarm = "OFF";
-          act_alarm = false;
-          digitalWrite (buzzerPin, LOW);
-          break;
+        lcd.print("ALARM IS ACTIVE");
+        for (int i = 0; i < 20; i++) {
+          if (button == KEYPAD_DOWN) {
+            stat_alarm = "OFF";
+            act_alarm = false;
+            digitalWrite (buzzerPin, LOW);
+            break;
+          }
+          bunyi();
         }
-
-      } else {
-
         lcd.clear();
         lcd.setCursor(0, 0);
-        sprintf(time, "%02i:%02i:%02i        ", hours, minutes, seconds);
-        lcd.print(time);
-        lcd.setCursor(12, 0);
-        lcd.print(tempC);
-        lcd.print((char)223);
-        lcd.print("C ");
-        lcd.setCursor(13, 1);
-        lcd.print(stat_alarm);
+        lcd.print("ALARM WILL BE");
         lcd.setCursor(0, 1);
-        sprintf(time, "%02i/%02i/%02i", year, month, days);
-        lcd.print(time);
+        lcd.print("POSTPONE");
+        act_alarm = false;
+        delay (1000);
+        act_alarm = true;
+        al_min = al_min + pp;
+        lcd.clear();
+        lcd.setCursor(1, 0);
+        lcd.print("POSTPONE");
+        /*else if (hours == al_hour && postmin == al_min) {
+           act_alarm == true;
+           al_min += pp;
+           break;
+           }*/
       }
 
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      sprintf(time, "%02i:%02i:%02i        ", hours, minutes, seconds);
+      lcd.print(time);
+      lcd.setCursor(12, 0);
+      lcd.print(tempC);
+      lcd.print((char)223);
+      lcd.print("C ");
+      lcd.setCursor(13, 1);
+      lcd.print(stat_alarm);
+      lcd.setCursor(0, 1);
+      sprintf(time, "%02i/%02i/%02i", year, month, days);
+      lcd.print(time);
       break;
     case DONE:
       lcd.setCursor(0, 0);
@@ -369,7 +377,7 @@ void printSetting() {
   }
 }
 
-// Increase the time model by one second
+
 void incTime() {
   // Increase seconds
   seconds++;
@@ -408,26 +416,26 @@ void printTime() {
     case SETDATE:
       lcd.setCursor(0, 1);
       switch (weekday) {
-        case SUN:
-          sprintf(time, "%02i/%02i/%02i     SUN", year, month, days);
+        case MIN:
+          sprintf(time, "%02i/%02i/%02i     MIN", year, month, days);
           break;
-        case MON:
-          sprintf(time, "%02i/%02i/%02i     MON", year, month, days);
+        case SEN:
+          sprintf(time, "%02i/%02i/%02i     SEN", year, month, days);
           break;
-        case TUE:
-          sprintf(time, "%02i/%02i/%02i     TUE", year, month, days);
+        case SEL:
+          sprintf(time, "%02i/%02i/%02i     SEL", year, month, days);
           break;
-        case WED:
-          sprintf(time, "%02i/%02i/%02i     WED", year, month, days);
+        case RAB:
+          sprintf(time, "%02i/%02i/%02i     RAB", year, month, days);
           break;
-        case THU:
-          sprintf(time, "%02i/%02i/%02i     THU", year, month, days);
+        case KAM:
+          sprintf(time, "%02i/%02i/%02i     KAM", year, month, days);
           break;
-        case FRI:
-          sprintf(time, "%02i/%02i/%02i     FRI", year, month, days);
+        case JUM:
+          sprintf(time, "%02i/%02i/%02i     JUM", year, month, days);
           break;
-        case SAT:
-          sprintf(time, "%02i/%02i/%02i     SAT", year, month, days);
+        case SAB:
+          sprintf(time, "%02i/%02i/%02i     SAB", year, month, days);
       }
       lcd.print(time);
       break;
@@ -512,3 +520,13 @@ float DS3231_get_treg()
 
   return rv;
 }
+
+void bunyi() {
+  digitalWrite (buzzerPin, HIGH);
+  delay (100);
+  digitalWrite (buzzerPin, LOW);
+  delay (1000);
+}
+
+
+
